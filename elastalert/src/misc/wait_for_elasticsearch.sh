b190b869591fba02011814efc14e4cbfa35319b2
Samuel <samuel@samuelbartag.com.br>
2019-07-02 17:35:40 -0300

    alteracao da image do elastalert

--- elastalert/src/misc/wait_for_elasticsearch.sh
+++ elastalert/src/misc/wait_for_elasticsearch.sh
@@ -4,8 +4,8 @@ RET=1
 while [[ RET -ne 0 ]]; do
   echo "Stallingwqwqwqw for Elasticsearch..."
   echo "http://$ELASTICSEARCH_HOST:$ELASTICSEARCH_PORT/"
-  echo $(curl -XGET -k "http://$ELASTICSEARCH_HOST:$ELASTICSEARCH_PORT/")
-  curl -XGET -k "http://$ELASTICSEARCH_HOST:$ELASTICSEARCH_PORT/" >/dev/null 2>&1
+  echo $(curl -X GET -k "http://$ELASTICSEARCH_HOST:$ELASTICSEARCH_PORT/")
+  curl -X GET -k "http://$ELASTICSEARCH_HOST:$ELASTICSEARCH_PORT/" >/dev/null 2>&1
   RET=$?
   sleep 5
 done
